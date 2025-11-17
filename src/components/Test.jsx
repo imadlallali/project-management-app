d4567d2549d60126f934cdc49c5eeaf9dfabbb22
imadlallali <imadlallali101@gmail.com>
2025-11-17 19:10:02 +0300

    testing pr

---
+++ src/components/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>Test</div>
+    const [imad, setImad] = React.useState('');
+    <button onClick={() => setImad('Imad')}>Click me</button>   
+    <h1>neyse</h1>
+  )
+}
